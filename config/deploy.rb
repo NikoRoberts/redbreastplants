66d1ae4239f764c5d5b0bd1abf73e8a58e63907c
Niko Roberts <niko@tasboa.com>
2016-12-20 15:13:56 +1100

    added dotenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :log_level, :debug
 set :pty, true
 
 set :linked_files, %w{config/database.yml .env}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
@@ -36,5 +36,6 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:cleanup'
+  after :finishing, 'deploy:restart'
 
 end
