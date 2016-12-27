d76cf933144dff9595b2192032c4ab44e43e61e8
Niko Roberts <niko@tasboa.com>
2016-12-27 22:07:36 +1100

    fixing email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,10 @@ class ContactMailer < ActionMailer::Base
   default to: "redbreast.plants@gmail.com"
 
   def notify_about_contact(name, from, website, content)
+    @name = name
+    @from = from
+    @website = website
+    @contact = contact
     mail(to: "redbreast.plants@gmail.com", subject: "Redbreast Contact Page - #{name}")
   end
 end
