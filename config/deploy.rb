71a827d8b57ddc16f8ea8eab679bcbf45617cdc8
Niko Roberts <niko@tasboa.com>
2018-09-30 20:39:38 +1000

    Generate sitemap on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'sitemap:generate'
+        run "RAILS_ENV=#{rails_env} rake sitemap:generate"
       end
     end
   end
